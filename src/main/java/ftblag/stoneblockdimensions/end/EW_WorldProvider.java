74049caea0b8cbabba8fb36d0984fe826bcf1449
modmuss50 <modmuss50@gmail.com>
2019-01-03 21:01:26 +0000

    EW_WorldProvider now extends WorldProviderEnd for better compatibility

--- src/main/java/ftblag/stoneblockdimensions/end/EW_WorldProvider.java
+++ src/main/java/ftblag/stoneblockdimensions/end/EW_WorldProvider.java
@@ -1,15 +1,17 @@
 package ftblag.stoneblockdimensions.end;
 
 import ftblag.stoneblockdimensions.StoneBlockDimensions;
+import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.util.math.MathHelper;
 import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.DimensionType;
-import net.minecraft.world.WorldProvider;
+import net.minecraft.world.WorldProviderEnd;
+import net.minecraft.world.end.DragonFightManager;
 import net.minecraft.world.gen.IChunkGenerator;
 
 import javax.annotation.Nullable;
 
-public class EW_WorldProvider extends WorldProvider {
+public class EW_WorldProvider extends WorldProviderEnd {
 
     @Override
     public IChunkGenerator createChunkGenerator() {
@@ -79,4 +81,32 @@ public class EW_WorldProvider extends WorldProvider {
     public DimensionType getDimensionType() {
         return StoneBlockDimensions.typeEW;
     }
+
+    //These methods are overridden from WorldProviderEnd to ensure that the dragon, doesnt spawn
+
+	@Override
+	public void onWorldSave() {
+		//Nope
+	}
+
+	@Override
+	public void onWorldUpdateEntities() {
+		//Nope
+	}
+
+	@Nullable
+	@Override
+	public DragonFightManager getDragonFightManager() {
+		return null; //No dragon for you!
+	}
+
+	@Override
+	public void onPlayerAdded(EntityPlayerMP player) {
+		//Nope
+	}
+
+	@Override
+	public void onPlayerRemoved(EntityPlayerMP player) {
+		//Nope
+	}
 }
